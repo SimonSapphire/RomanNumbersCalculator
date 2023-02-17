1365da1fbc1d2a5ca73894e74884824b799aa72e
SimonSapphire <s.druchinin@stud.sibguti.ru>
2023-02-17 22:08:40 +0700

    Added RomanNumbersCalculator App

---
+++ RomanNumbersCalculator/Models/RomanNumberException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumbersCalculator.Models
+{
+    internal class RomanNumberException: Exception
+    {
+        private const string errorMessage = "#ERROR";
+        public RomanNumberException(string message=errorMessage) : base(message) { }
+    }
+}
